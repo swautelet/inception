f4388e386e53233a39e2690911e840a3c87dea1d
SWautele <swautele@student.s19.be>
2023-02-14 16:56:51 +0100

    save

--- srcs/wordpress/wp.sh
+++ srcs/wordpress/wp.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# if [ -f "/var/www/html/wordpress/wp-config.php" ]; then
-    # echo "Downloading Wordpress"
-    # mkdir -p /var/www/html/
-    # wget https://wordpress.org/latest.tar.gz -O /tmp/wordpress.tar.gz
-    # tar xzf /tmp/wordpress.tar.gz -C /var/www/html/
-    # cp -R /var/www/html/wordpress/* /var/www/html
-    # rm -rf /tmp/wordpress.tar.gz
-    # rm -rf /var/www/html/wordpress
+if [ -f "/var/www/html/wordpress/wp-config.php" ]; then
+    echo "Downloading Wordpress"
+    mkdir -p /var/www/html/
+    wget https://wordpress.org/latest.tar.gz -O /tmp/wordpress.tar.gz
+    tar xzf /tmp/wordpress.tar.gz -C /var/www/html/
+    cp -R /var/www/html/wordpress/* /var/www/html
+    rm -rf /tmp/wordpress.tar.gz
+    rm -rf /var/www/html/wordpress
 
     sed -i "s/username_here/$DB_LOGIN/g" wp-config-sample.php
     sed -i "s/password_here/$DB_PASSWORD/g" wp-config-sample.php
@@ -17,8 +17,8 @@
     wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=supervisor --admin_password=123456789 --admin_email=info@example.com --allow-root
     wp user create "$WP_ADMIN_LOGIN" "$WP_ADMIN_EMAIL" --user_pass="$WP_ADMIN_PASSWORD" --role=administrator --allow-root
     wp theme install inspiro --activate --allow-root
-# else
-#     echo "Download failed"
-# fi
+else
+    echo "Download failed"
+fi
 
 exec "$@"
