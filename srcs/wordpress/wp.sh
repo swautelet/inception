8953db81c11fdd8966355e47a3574cba11ac6098
swautele <swautle@student.s19.com>
2023-02-16 13:56:23 +0100

    save

--- srcs/wordpress/wp.sh
+++ srcs/wordpress/wp.sh
@@ -15,7 +15,7 @@ if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
     # sed -i "s/localhost/$DB_HOSTNAME/g" wp-config-sample.php
     # sed -i "s/database_name_here/$DB_NAME/g" wp-config-sample.php
     # cp wp-config-sample.php wp-config.php
-    wp config create --dbname=$DB_NAME --dbuser=$DB_LOGIN --dbpass=$DB_PASSWORD --dbhost=$DB_HOSTNAME
+    wp config create --dbname=$DB_NAME --dbuser=$DB_LOGIN --dbpass=$DB_PASSWORD --dbhost=$DB_HOSTNAME --allow-root
     wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=supervisor --admin_password=123456789 --admin_email=info@example.com --allow-root
     wp user create "$WP_ADMIN_LOGIN" "$WP_ADMIN_EMAIL" --user_pass="$WP_ADMIN_PASSWORD" --role=administrator --allow-root
     wp theme install inspiro --activate --allow-root
